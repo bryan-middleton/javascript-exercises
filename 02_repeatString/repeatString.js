f8a6e99e41e6fc28471f5b8550441a5f64f879b7
Bryan Middleton <bryanmiddleton1986@gmail.com>
2022-11-11 11:54:28 +0000

    js exercises 1-3 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(message, number) {
+    
+    let string='';
+    if (number<0){
+        string='ERROR';
+    }
+    else{
+        for (let i = 0; i<number; i++){ 
+            string+=message;
+        }
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const splitString = input.split("");
+    let reversed = '';
+    for (let i=splitString.length-1;i>=0;i--){
+        reversed+=splitString[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
