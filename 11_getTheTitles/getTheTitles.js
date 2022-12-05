bbf522d424dffd5b61d1acf9e32cd26d8d8045c5
Bryan Middleton <bryanmiddleton1986@gmail.com>
2022-12-05 18:16:34 +0000

    complete exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+        const titles = books.map(book => book.title);
+        console.table(titles);
+        return titles;
+    }
 
 // Do not edit below this line
 module.exports = getTheTitles;
